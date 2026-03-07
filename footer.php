35022072a0f556c6c7b7b08374cebb1156c9b56c
alnubvar <alnub_work@mail.ru>
2026-03-07 12:13:55 +0300

    deleted the feedback block test at the bottom

--- footer.php
+++ footer.php
@@ -1,8 +1,4 @@
 <footer>
-    <div class="contact-form">
-        <h2>Обратная связь</h2>
-        <?php echo do_shortcode('[contact-form-7 id="ca7b288" title="Обратная связь"]'); ?>
-    </div>
     <div class="container">
         <p>Rocket Business © <?php echo date('Y'); ?></p>
     </div>
